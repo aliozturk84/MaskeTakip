6798872b7a46e250c1a18aad63c3360c9759d2dd
aliiozturk <aliozturkinfo@gmail.com>
2024-02-11 17:58:04 +0300

    Add project files.

---
+++ Business/Concreate/PersonManager.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concreate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concreate
+{
+    public class PersonManager
+    {
+        public void ApplyForMask(Person person)
+        {
+
+        }
+
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workaround
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string mesaj = "Selam";
+            //double tutar = 10000;
+            //int sayi = 100;
+            //bool girisYapmısMi = false;
+
+            //string ad = "Engin";
+            //string soyad = "Demiroğ";
+            //int dogumYili = 1985;
+            //long tcNO = 12345678910;
+
+
+            //Console.WriteLine(tutar * 1.18);
+
+            //Console.WriteLine(tutar * 1.18);
+
+            Vatandas vatandas = new Vatandas();
+            
+
+
+            Console.ReadLine();
+        }
+
+
+
+    }
+    
+    public class Vatandas
+    {
+        public string Ad { get; set; }
+        public string Soyad { get; set; }
+        public int DogumYili { get; set; }
+        public long TcNo { get; set; }
+    }
+}
